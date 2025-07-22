c10cb4404bd5549a845608382b4cea0a2eff79a2
kikomonteroplata@gmail.com <kikomonteroplata@gmail.com>
2025-07-22 17:29:57 +0200

    listas

---
+++ 04_Listas/pruebas.py
@@ -0,0 +1,8 @@
+paises = ['Germany', 'France','Belgium','Sweden','Denmark','Finland','Norway','Iceland','España']
+gr, fr, bg, sw, *scandic, es = paises
+print(gr)
+print(fr)
+print(bg)
+print(sw)
+print(scandic)
+print(es)
